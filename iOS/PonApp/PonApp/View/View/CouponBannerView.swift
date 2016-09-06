b3955fb0c62ddbead7c7408d278001fa63edce04
HaoLe <thienhaole92@gmail.com>
2016-09-06 17:48:20 +0700

    edit coupon vc

--- iOS/PonApp/PonApp/View/View/CouponBannerView.swift
+++ iOS/PonApp/PonApp/View/View/CouponBannerView.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class CouponBannerView: UIView {
 
+    var bottomBorderColor: UIColor = UIColor(red:200.0/255.0, green:210.0/255.0, blue:214.0/255.0, alpha:1)
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.initialize()
@@ -33,6 +35,13 @@ class CouponBannerView: UIView {
         let rectangle = CGRectMake(0,rect.size.height * (20/95), rect.size.width, rect.size.height - (rect.size.height * (20/95)))
         CGContextAddRect(context, rectangle)
         CGContextFillPath(context)
+        
+        
+        CGContextSetLineWidth(context, 1.0)
+        CGContextSetStrokeColorWithColor(context, bottomBorderColor.CGColor)
+        CGContextMoveToPoint(context, 15, rect.size.height)
+        CGContextAddLineToPoint(context, rect.size.width, rect.size.height)
+        CGContextStrokePath(context)
     }
 
 }
