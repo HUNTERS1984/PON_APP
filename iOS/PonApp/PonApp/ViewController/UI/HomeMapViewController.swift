9d2f1850dfc8301e02019edcb8cdfc3ccf8fefea
HaoLe <thienhaole92@gmail.com>
2016-09-06 22:50:55 +0700

    setup map view

--- iOS/PonApp/PonApp/ViewController/UI/HomeMapViewController.swift
+++ iOS/PonApp/PonApp/ViewController/UI/HomeMapViewController.swift
@@ -53,15 +53,66 @@ extension HomeMapViewController {
 extension HomeMapViewController {
     
     private func showOfferView() {
-        self.offerViewBottomConstraint.constant += 172
-        self.hideOfferButton.hidden = false
-        self.menuButton.hidden = true
+        UIView.animateWithDuration(0.5) { 
+            self.offerViewBottomConstraint.constant += 172
+            self.hideOfferButton.hidden = false
+            self.menuButton.hidden = true
+            self.view.layoutIfNeeded()
+        }
     }
     
     private func hideOfferView() {
-        self.offerViewBottomConstraint.constant -= 172
-        self.hideOfferButton.hidden = true
-        self.menuButton.hidden = false
+        UIView.animateWithDuration(0.5) {
+            self.offerViewBottomConstraint.constant -= 172
+            self.hideOfferButton.hidden = true
+            self.menuButton.hidden = false
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+}
+
+//MARK: - UICollectionViewDataSource
+extension HomeMapViewController: UICollectionViewDataSource {
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CouponCollectionViewCell", forIndexPath: indexPath) as! CouponCollectionViewCell
+        return cell
+        
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        
+        let commentView = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: "CouponCollectionViewCell", forIndexPath: indexPath) as! CouponCollectionViewCell
+        return commentView
+    }
+    
+}
+
+//MARK: - UICollectionViewDelegate
+extension HomeMapViewController: UICollectionViewDelegate {
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let vc = CouponViewController.instanceFromStoryBoard("Coupon")
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+}
+
+//MARK: - UICollectionViewDelegateFlowLayout
+extension HomeMapViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(150, 160)
     }
     
 }
