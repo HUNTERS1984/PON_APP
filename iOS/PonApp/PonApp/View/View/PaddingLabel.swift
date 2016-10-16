d077245665263c07ae350375a6d64d66769dc9bb
HaoLe <thienhaole92@gmail.com>
2016-10-16 11:43:21 +0700

    update UI

---
+++ iOS/PonApp/PonApp/View/View/PaddingLabel.swift
@@ -0,0 +1,31 @@
+//
+//  PaddingLabel.swift
+//  PonApp
+//
+//  Created by OSXVN on 10/16/16.
+//  Copyright © 2016 HaoLe. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class PaddingLabel: UILabel {
+    
+    @IBInspectable var topInset: CGFloat = 5.0
+    @IBInspectable var bottomInset: CGFloat = 5.0
+    @IBInspectable var leftInset: CGFloat = 5.0
+    @IBInspectable var rightInset: CGFloat = 5.0
+    
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+        super.drawText(in: UIEdgeInsetsInsetRect(rect, insets))
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        get {
+            var contentSize = super.intrinsicContentSize
+            contentSize.height += topInset + bottomInset
+            contentSize.width += leftInset + rightInset
+            return contentSize
+        }
+    }
+}
